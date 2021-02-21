e1b55e19a8604fbb033371b46125474dcbf4f9e2
Nghi Le <vinhnghi223@gmail.com>
2021-02-21 16:48:02 +0200

    3.10 phonebook backend step10: deployed app to heroku

--- frontend/part2/phonebook/src/services/PeopleService.js
+++ frontend/part2/phonebook/src/services/PeopleService.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://fso-backend-1.herokuapp.com/api/persons'
 
 const getAll = () => {
     return axios
