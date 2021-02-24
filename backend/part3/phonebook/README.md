9ceeac38ff34e721c405765b64bbeac6cc422423
Nghi Le <vinhnghi223@gmail.com>
2021-02-24 18:10:20 +0200

    finalized part3b

---
+++ backend/part3/phonebook/README.md
@@ -0,0 +1,8 @@
+`npm run build:ui`
+builds the frontend and copies the production version under the backend repository.
+
+`npm run deploy:full`
+combines these two and contains the necessary git commands to update the backend repository.
+
+`npm run logs:prod`
+show the heroku logs.
---
+++ backend/part3/phonebook/build/precache-manifest.012f3532f0ea3c48def6f09e3b5fa66c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d3efc28f6db913efd5ec8eca6aba7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d4f12017fba3986454",
+    "url": "/static/js/main.9969f78d.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
