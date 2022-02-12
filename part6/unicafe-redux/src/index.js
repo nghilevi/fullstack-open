aa290a3b68ed67bd20009e11c6b474d9331bdcff
Nghi Le <vinhnghi223@gmail.com>
2022-02-12 18:24:04 +0200

    added 6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -34,9 +34,11 @@ const App = () => {
       <button onClick={ok}>neutral</button> 
       <button onClick={bad}>bad</button>
       <button onClick={reset}>reset stats</button>
+
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+
     </div>
   )
 }
